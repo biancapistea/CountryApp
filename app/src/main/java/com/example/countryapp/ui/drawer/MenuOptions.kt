c976ef8789858e34e33125344fd945d622144fbd
Bianca Pistea <bianca.pistea@nagarro.com>
2023-10-20 16:14:31 +0300

    refactor code

--- app/src/main/java/com/example/countryapp/ui/drawer/MenuOptions.kt
+++ app/src/main/java/com/example/countryapp/ui/drawer/MenuOptions.kt
@@ -28,12 +28,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.countryapp.R
 import kotlinx.coroutines.launch
